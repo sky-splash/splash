8ff5f2d51f83442ecb4bade8a806337d2a1423cd
Faisal <faisaldilshadwork@gmail.com>
2024-07-28 14:47:56 +0530

    Ice added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,23 @@
-import React from "react";
 import bg from "../assets/hero-image.jpg";
 import bottle3 from "../assets/bottel3.png";
+import ice1 from "../assets/ice1.png";
+import ice2 from "../assets/ice2.png";
+import ice3 from "../assets/ice3.png";
+import ice4 from "../assets/ice4.png";
 
 const Hero = () => {
   return (
     <div
       className="bg-cover bg-center h-screen flex items-center justify-center"
       style={{ backgroundImage: `url(${bg})` }}>
-      <div className="flex w-full h-full">
+      <div className="flex w-full h-full px-7">
         {/* Left Half */}
         <div className=" flex items-center  w-1/2  pl-16">
           <div className="flex flex-col justify-center pl-20">
-            <button className="mb-1 bg-white p-4 w-[15rem] rounded-[6rem] font-bold text-[#0A75AD]">
+            <button className=" bg-white px-6 py-1 w-[15rem] rounded-[6rem] font-bold text-[#0A75AD]">
               MINERAL COMPOSITION
             </button>
-            <h1 className="text-[5rem] mb-4 font-bold font">
+            <h1 className="text-[5rem] mb-4 font-bold leading-[7rem]">
               Pure & Healthy Drinking Water
             </h1>
             <div className="flex space-x-5">
@@ -28,8 +31,32 @@ const Hero = () => {
           </div>
         </div>
         {/* Right Half */}
-        <div className="flex items-center justify-center w-1/2">
-          <img src={bottle3} alt="Bottle" className="max-w-full h-auto" />
+        <div className="flex items-center justify-center w-1/2 relative px-[9rem]">
+          <img
+            src={bottle3}
+            alt="Bottle"
+            className="max-w-full h-auto relative z-10"
+          />
+          <img
+            src={ice1}
+            alt=""
+            className="absolute bottom-[9rem] left-0 z-0"
+          />
+          <img
+            src={ice2}
+            alt=""
+            className="absolute bottom-[9rem] left-[12rem] z-20"
+          />
+          <img
+            src={ice3}
+            alt=""
+            className="absolute bottom-[9rem] right-[12rem] z-20"
+          />
+          <img
+            src={ice4}
+            alt=""
+            className="absolute bottom-[9rem] right-0 z-0"
+          />
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
 import { FaPhone } from "react-icons/fa";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 2) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="flex  justify-around items-center w-full font-montserrat font-semibold">
-      <img src={logo} alt="" className="h-[5rem]" />
-      <ul className="flex space-x-10">
-        <li>Home</li>
-        <li>pages</li>
-        <li>Services</li>
-        <li>Project</li>
-        <li>Blog</li>
-        <li>Contact us</li>
-      </ul>
-      <div className="flex items-center space-x-6">
-        <FaPhone className="text-3xl" />
-        <h1>+99123456293</h1>
+    <div
+      className={`fixed w-full top-0 left-0 z-50 transition-colors duration-300 ${
+        isScrolled ? "bg-white shadow-lg" : "bg-transparent"
+      }`}>
+      <div className="flex  justify-around items-center w-full font-montserrat font-semibold ">
+        <img src={logo} alt="" className="h-[5rem]" />
+        <ul className="flex space-x-10">
+          <li>Home</li>
+          <li>pages</li>
+          <li>Services</li>
+          <li>Project</li>
+          <li>Blog</li>
+          <li>Contact us</li>
+        </ul>
+        <div className="flex items-center space-x-6">
+          <FaPhone className="text-3xl" />
+          <h1>+99123456293</h1>
+        </div>
       </div>
     </div>
   );
